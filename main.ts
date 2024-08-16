430e8f01670e81419409349c89c32112c723dd61
Welder Askowiks <72799464+askowiks@users.noreply.github.com>
2024-08-16 13:45:18 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    robotbit.Servo(robotbit.Servos.S1, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    robotbit.Servo(robotbit.Servos.S1, 180)
+})
+basic.forever(function () {
+	
+})
